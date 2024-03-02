e220c7c160024d0111fe69d3c7365f42cab8d080
ben <ben@benpiper.com>
2024-03-02 09:49:49 -0500

    clean up

--- controllers/serverinfo.py
+++ controllers/serverinfo.py
@@ -1,52 +1,60 @@
-from fastapi import APIRouter, Depends, HTTPException, Security
+"""serverinfo.py"""
 import socket
 import os
+from fastapi import APIRouter
+
 
 router = APIRouter()
+
 
 @router.get("/hostname")
 def get_hostname():
     return socket.gethostname()
+
 
 @router.get("/ip")
 def get_ipv4():
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     return sock.getsockname()[0]
 
+
 @router.get("/port")
 def get_port():
     return "Not implemented"
 
+
 @router.get("/environment")
 def get_env():
-	return(getserverparams())
+    return (getserverparams())
+
 
 @router.get("/environment/{key}")
 def get_env(key: str):
-	return(getserverparam(key))
+    return (getserverparam(key))
+
 
 def getserverparams():
     environmentvars = {}
     for each in os.environ.keys():
         environmentvars[each] = os.environ[each]
-        
+
     return environmentvars
 
 
 def getserverparam(param_name):
+    """Get environment variables"""
+    for each in os.environ.keys():
+        # Uncomment the next line if you want to see all of the values that could be used
+        print(each, os.environ[each]), "<br>"  # db
+        # print each ":each <br>" #db
 
-    for each in os.environ.keys():	
-		#Uncomment the next line if you want to see all of the values that could be used
-        print (each, os.environ[each]), "<br>" #db
-        #print each ":each <br>" #db
-    
-    for each in os.environ.keys():	
-	    each_value = os.environ[each]
-	    each_value_string = str(each_value)
-	    each_string = str(each)
+    for each in os.environ.keys():
+        each_value = os.environ[each]
+        each_value_string = str(each_value)
+        each_string = str(each)
 
-	    if each_string == param_name:			
-		    servervalue = each_value_string
-		    return servervalue
+        if each_string == param_name:
+            servervalue = each_value_string
+            return servervalue
 
     return None
--- main.py
+++ main.py
@@ -1,31 +1,42 @@
-from fastapi import FastAPI, Path
-from typing import Annotated
-from controllers.serverinfo import router as ServerinfoRouter
-import uvicorn
+"""main.py"""
 import string
 import random
-import socket
 import time
+from typing import Annotated
+import uvicorn
+from fastapi import FastAPI, Path, HTTPException
+from controllers.serverinfo import router as ServerinfoRouter
 
 app = FastAPI()
 
 app.include_router(ServerinfoRouter, prefix="/serverinfo")
 
+
 @app.get('/')
 def home():
-    return 'Hello World!'
+    """Hello world"""
+    raise HTTPException(status_code=404, detail="Nothing here")
+
+
 
 @app.get('/random')
 def get_random():
+    """Generates a string of random characters"""
     return ''.join(random.choice(string.printable) for _ in range(6291456))
 
+
 @app.get('/random/{length}')
-def get_random(length: Annotated[int, Path( le=1024)]):
+def get_random_length(length: Annotated[int, Path(le=1024)]):
+    """Generates a string of random characters of length"""
     return ''.join(random.choice(string.printable) for _ in range(length))
+
 
 @app.get('/delay/{seconds}')
 def get_delay(seconds: int):
+    """Delay for seconds"""
     time.sleep(seconds)
+    return {"message": f"Wait of {seconds} complete"}
+
 
 if __name__ == '__main__':
     uvicorn.run("main:app", host='0.0.0.0', port=5000, reload=True)
